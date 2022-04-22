8ea7704195c3b64dd6d533a83c0a6f563b237feb
fergalax <87350314+fergalax@users.noreply.github.com>
2022-04-22 13:24:53 -0500

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var frutas = ["jicama","pera","yaca","mango"]; 
+
+var masFrutas = frutas.push("coco"); //para añadir más elementos a un arreglo
+var eliminacion = frutas.pop ("coco"); //para eliminar un elemento del arreglo
+var nuevoArreglo = frutas.unshift("marcelo");//añade un elemento nuevo en el arreglo al  inicio
+var eliminarFruta = frutas.shift("marcelo");//elimina elemento princpial en el arreglo
+var posicion = frutas.indexOf("pera");//este se puede cambiar y mandarlo a llamar desde un cosole.log
+
+
+
+console.log(frutas);
+console.log (frutas.length) //para saber cuantos elementos hay en el arreglo
+console.log (frutas[2])//posición en un elemento del arreglo
+console.log(posicion) //buscar el index de ciertos elementos en el arreglo
+
+
+
+
+
+
+
